70a07e96aafa3b021077220d69b5d0006875ce2b
Umm e rabab ali <rafiabaloch454@gmail.com>
2025-06-02 15:38:42 -0700

    Add files via upload

---
+++ smit assignemnt o1/html/app.js
@@ -0,0 +1,72 @@
+// chapert no 01 (alert)
+
+// question no 01: write a script to greet your website visitor using JS alert box?
+alert("welcome to my website!");
+
+// question no 02: write a script to display following message on you web page?
+alert("Error! Please enter a valid password.");
+
+// question no 03: write a script to display following message on your web page?
+alert("Welcome to JS Land ... \n Happy coding!");
+
+// question no 04: write a script to display following message in sequence?
+alert("Welcome to JS Land...");
+alert("Happy coding!");
+
+// question no 05: generate the following message throught brower's developerr console?
+console.log("Hello... I can run JS through my web brower's console");
+
+// question no 07: pracice placement of <script> </script> element in following section of your project in excercise 6:?
+// 1. in head section 
+// 2. in body section before your page's HTML
+// 3. in body section after your page's HTML
+// 4. in body section at the end of your page's HTML
+
+// chapter no 02 ( variable for String)
+
+// question no 01:  declear a variable called username?
+var username;
+
+// question no 02: declear a variable called myName and assign to it a string that represent your full name?
+var myName = "Umm e rabab ali";
+
+// question no 03: write script to 
+// 1. declear a JS variable, titled Message.
+var message;
+// 2. assign "Hello world" to the variable message.
+message = " Hello world";
+// 3. alert the message variable?
+alert("message");
+
+// question no 04: write a script to save student's bio data in JS variables and show the data in alert boes?
+var studentName = " Umm e rabab ali";
+var studentAge = " 21 years old";
+var studentCourse = " Certified Mobile Appliction Development";
+alert("student Name" + studentName);
+alert("student age" + studentAge);
+alert("student course" + studentCourse);
+
+// question no 05: write a script to display the following alert using JS?
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert("pizza " + pizza);
+
+// question no 06: -declear a variable called email and assign to it a string that represent your email adress 
+// e.g example@example.com). show the below mentioned message in an alert box (hint: use string concatenation)?
+var email = "rafiabaloch@454.com";
+alert("my email adress is " + email);
+
+// question no 07: delcarea variable called book and give it the valve
+//  "A smarter way to learn JavaScript". display the following message in an alert box?
+var book = " A smater way to learn JavaScript ";
+alert("I am trying to learn from the book " + book);
+
+// question no 08: write a script to display this in brower's through JS?
+document.write("Yah! I can write HTML content through JavaScript");
+
+                // The first (SMIT)assignment of end of chapter 1 and 2 ...!
+
+
+
+
+
+
